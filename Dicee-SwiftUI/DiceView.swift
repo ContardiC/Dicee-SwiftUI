fa1d7595aeda4fb17e680dd49eb2d5ae0a94f1af
Carlo Contardi <c.contardi@spacecoding.it>
2025-03-17 17:03:20 +0100

    add: @State wrapper

---
+++ Dicee-SwiftUI/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Carlo Contardi on 17/03/25.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
